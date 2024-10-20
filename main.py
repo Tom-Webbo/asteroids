abf26567f7f30e1f34889f439ba9fa38a1bbfadb
Tom-Webbo <Tom.webbo@hotmail.co.uk>
2024-10-20 18:04:50 +0100

    Added player and circle classes, added player class to main.py

--- main.py
+++ main.py
@@ -3,23 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print("Screen width:",SCREEN_WIDTH)
-    print("Screen height:",SCREEN_HEIGHT)
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        #render
         
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        
+
+        #fps limit
+        dt = clock.tick(60) / 1000
     pygame.quit()
 
 
