4c46d87f72793597f10fc5de89cb248415f60bf4
Tom-Webbo <Tom.webbo@hotmail.co.uk>
2024-10-20 17:37:40 +0100

    added constants and pygame code

--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("black")
+        pygame.display.flip()
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
